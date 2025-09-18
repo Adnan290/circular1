1ada0d769cb1e231fc287c5fa84f63dd827c1cb3
Adnan290 <57295691+Adnan290@users.noreply.github.com>
2025-09-18 16:23:11 +0500

    Add files via upload

--- OTA.py
+++ OTA.py
@@ -3,76 +3,40 @@ import M5
 from M5 import *
 import m5ui
 import lvgl as lv
-import network 
-#import urequests
-import machine
-import time
-# ---------- WiFi & OTA ----------
-SSID = "Redmi Note 10"
-PASSWORD = "12345678"
-URL = "https://raw.githubusercontent.com/Adnan290/circular1/main/main.py"
 
-def wifi_connect():
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(SSID, PASSWORD)
-    print("Connecting WiFi...")
-    for _ in range(20):
-        if wlan.isconnected():
-            print("Connected:", wlan.ifconfig())
-            return True
-        time.sleep(0.5)
-    print("WiFi connect failed")
-    return False
-
-def ota_update():
-    if not wifi_connect():
-        return
-    try:
-        print("Downloading:", URL)
-        r = urequests.get(URL)
-        if r.status_code == 200:
-            with open("main.py", "w") as f:
-                f.write(r.text)
-            print("Update OK, rebooting...")
-            time.sleep(2)
-            machine.reset()
-        else:
-            print("HTTP error:", r.status_code)
-        r.close()
-    except Exception as e:
-        print("OTA failed:", e)
-# --------------------------------
 
 
 page0 = None
+label0 = None
+
 
 def setup():
-    global page0
-    M5.begin()
-    Widgets.setRotation(1)
-    m5ui.init()
-    page0 = m5ui.M5Page(bg_c=0xffffff)
-    page0.screen_load()
+  global page0, label0
+
+  M5.begin()
+  Widgets.setRotation(1)
+  m5ui.init()
+  page0 = m5ui.M5Page(bg_c=0xffffff)
+  label0 = m5ui.M5Label("label0", x=166, y=58, text_c=0x000000, bg_c=0xffffff, bg_opa=0, font=lv.font_montserrat_14, parent=page0)
+
+  page0.screen_load()
+
 
 def loop():
-    global page0
-    M5.update()
+  global page0, label0
+  M5.update()
+  label0.set_text(str('ADNAN'))
 
 
 if __name__ == '__main__':
-    print("Starting OTA updater...")
-    ota_update()   # will reboot if update succeeds
-
-    # If no update happened, start UI
+  try:
+    setup()
+    while True:
+      loop()
+  except (Exception, KeyboardInterrupt) as e:
     try:
-        setup()
-        while True:
-            loop()
-    except (Exception, KeyboardInterrupt) as e:
-        try:
-            m5ui.deinit()
-            from utility import print_error_msg
-            print_error_msg(e)
-        except ImportError:
-            print("please update to latest firmware")
+      m5ui.deinit()
+      from utility import print_error_msg
+      print_error_msg(e)
+    except ImportError:
+      print("please update to latest firmware")
